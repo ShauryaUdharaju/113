ebd9aff0b40a4bb9e3574bcf30dd2d1ca1b55d61
ShauryaUdharaju <118858893+ShauryaUdharaju@users.noreply.github.com>
2024-07-19 16:48:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function preload()
+{
+
+}
+    
+
+function draw() 
+{
+   
+}
+
+function take_snapshot() {
+    save('myFilterImage.png')
+}
